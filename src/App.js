604bf49aabf04799ee6465120cb7cb06e48681b7
Isaac Alter <i_alter@yahoo.com>
2023-09-23 19:42:41 -0600

    Add feat: Basic structure successfully setup. All five pages working and displaying headers. GeneralSetup is closed and will be merged to main after being pushed.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import AboutMe from './AboutMe';
 import Contact from './Contact';
 import Projects from './Projects';
 import CodeChallenge from './CodeChallenge';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={AboutMe} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/code-challenge" component={CodeChallenge} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/code-challenge" element={<CodeChallenge />} />
+      </Routes>
     </Router>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-}
+};
 
 export default App;
