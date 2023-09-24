11f924f6f5d1864baf3afbc4cfdfab94a03e557a
Isaac Alter <i_alter@yahoo.com>
2023-09-23 18:46:08 -0600

    Add feat: App.js has routes set up using BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '/.Home';
+import AboutMe from '/.AboutMe';
+import Contact from '/.Contact';
+import Projects from '/.Projects';
+import CodeChallenge from '/.CodeChallenge';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={AboutMe} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/code-challenge" component={CodeChallenge} />
+    </Router>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
