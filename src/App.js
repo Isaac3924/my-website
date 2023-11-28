8fc3b2d38373fa0e3063a2b5fe87228fe5823dc0
Isaac Alter <i_alter@yahoo.com>
2023-11-28 15:27:02 -0700

    Add feat: removed contacts route, javascript file, and all relations to it as it is no longer needed with contacts icons in the corner. Further, adjusted bottom menu height thanks to free space and not wanting it to obscure home page text when visible.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import AboutMe from './AboutMe';
-import Contact from './Contact';
 import Projects from './Projects';
 import CodeChallenges from './CodeChallenges';
 import TLDR from './projects/TLDR';
@@ -31,7 +30,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<AboutMe />} />
-            <Route path="/contact" element={<Contact />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/code-challenges" element={<CodeChallenges />} />
             <Route path="/projects/tldr" element={<TLDR/>} />
