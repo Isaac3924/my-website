65764c0d51f18934a9ae0fd1f73cd99f8efb6a90
Isaac Alter <i_alter@yahoo.com>
2024-01-02 12:34:13 -0700

    Add feat: new css logic to make the text of a page opaque when the grid is active, and adjusted names of classes and ids to better explain roles. Put background into own div element and set position and dimensions as needed.

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,15 +5,19 @@ import GridBackground from "./components/animeGridBackground";
 function AboutMe() {
   const { menuVisible } = useMenu();
   return (
-    <div className={`page ${menuVisible ? 'menu-visible' : ''} aboutme`}>
-      <div className="page-head">
-        <h1 className="page-title">About</h1>
+    <div id="aboutMe" className={`page ${menuVisible ? 'menu-visible' : ''} aboutme`}>
+      <div className="page-text">
+        <div className="page-head">
+          <h1 className="page-title">About</h1>
+        </div>
+        <div className="page-content">
+          <p className="about-summary">Hi, I'm Isaac Alter, and I am a web developer specializing in Back-End development currently based in Colorado Springs, Colorado. I've worked in a variety of langugages and tech stacks, ranging from the Ruby on Rails framework, Python, C#, C++, and Java. I am also familiar with PostgreSQL, MySQL, HTML, CSS, JavaScript, and the React framework. Additionally, I am a big fan of stories and games, especially manga and any sort of tabletop or video game! I use these stories and experiences to inspire my work and consider new ideas and challenges for myself, and am always seeking new concepts and views to further my own knowledge.</p>
+          <p className="about-contact">If you would like to speak further, feel free to reach me via my LinkedIn at the top right of the page, or email me here. Feel free to look at my projects page to see my work, see what sorts of code challenges I am taking up and practicing, or browse my GitHub also reachable from the top right of the page.</p>
+        </div>
       </div>
-      <div className="page-content">
-        <p className="about-summary">Hi, I'm Isaac Alter, and I am a web developer specializing in Back-End development currently based in Colorado Springs, Colorado. I've worked in a variety of langugages and tech stacks, ranging from the Ruby on Rails framework, Python, C#, C++, and Java. I am also familiar with PostgreSQL, MySQL, HTML, CSS, JavaScript, and the React framework. Additionally, I am a big fan of stories and games, especially manga and any sort of tabletop or video game! I use these stories and experiences to inspire my work and consider new ideas and challenges for myself, and am always seeking new concepts and views to further my own knowledge.</p>
-        <p className="about-contact">If you would like to speak further, feel free to reach me via my LinkedIn at the top right of the page, or email me here. Feel free to look at my projects page to see my work, see what sorts of code challenges I am taking up and practicing, or browse my GitHub also reachable from the top right of the page.</p>
+      <div className="page-background">
+        <GridBackground/>
       </div>
-      <GridBackground/>
     </div>
   );
 };
