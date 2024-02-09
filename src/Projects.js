2c70241d50904306d5f9dd5422e9e74c89d44e0c
Isaac Alter <i_alter@yahoo.com>
2024-02-09 00:21:11 -0700

    add refactor: selection animations for peoject list in projects working as intended for now

--- src/Projects.js
+++ src/Projects.js
@@ -4,19 +4,56 @@ import { useMenu } from "./MenuContext";
 function Projects() {
   const { menuVisible } = useMenu();
   return (
-    <div className={`page ${menuVisible ? 'menu-visible' : ''}`}>
-      <div className="page-head">
-        <h1 className="page-title">Projects</h1>
-      </div>
-      <div className="page-list">
-        <ul className="list-projects">
-          <li>
-            <a href="projects/tldr">TL:DR</a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/isaacalter/">LinkedIn</a>
-          </li>
-        </ul>
+    <div id="projects" className={`page ${menuVisible ? 'menu-visible' : ''}`}>
+      <div className="page-content">
+        <div className="page-head">
+          <h1 className="page-title">Projects</h1>
+        </div>
+        <div className="projects">
+          <div className="projects-image-viewer">
+            <div className="projects-image-viewer-content">
+              <div className="projects-image-wrapper">
+                <div id="TLDR" className="projects-image-size">
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects-list-viewer">
+            <div className="projects-list-top-wrapper">
+              <div className="projects-list-header">
+                <hr className="projects-separator"></hr>
+              </div>
+            </div>
+            <ul className="projects-list">
+              <li>
+                <a href="projects/tldr">
+                  <div className="projects-row">
+                    <div className="projects-row-left">
+                      <h3>TL:DR</h3>
+                    </div>
+                    <div className="projects-row-right">
+                      <h4>Python Web Application</h4>
+                    </div>
+                  </div>
+                </a>
+
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/isaacalter/">
+                <div className="projects-row">
+                    <div className="projects-row-left">
+                      <h3>LinkedIn</h3>
+                    </div>
+                    <div className="projects-row-right">
+                      <h4>REPLACE_ME</h4>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
