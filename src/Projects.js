4e089fd7c1757a36b8438d9b0dc032a0a2d0a5f1
Isaac Alter <i_alter@yahoo.com>
2024-03-06 21:08:32 -0700

    add refactor: added in further functionality and images for the individual project pages. Projects page done for now unless further refactor needed.

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { useMenu } from "./MenuContext";
-import TLDR from './pictures/tldr.png';
-import OTHER from './pictures/github.png'
+import tldr from './pictures/tldr.png';
+import whether_sweater from './pictures/whether_sweater.png'
+import tea_subs from './pictures/tea_subs.png'
+import viewing_party from './pictures/viewing_party.png'
+import little_esty_shop from './pictures/little_esty_shop.png'
 
 function Projects() {
   const { menuVisible } = useMenu();
@@ -18,11 +21,11 @@ function Projects() {
   };
 
   const projects_Array = [
-    ["tldr", "TL:DR", "Python Web Application"],
-    ["address_here", "LinkedIn", "REPLACE_ME"],
-    ["address_here", "Whether-Sweater", "REPLACE_ME"],
-    ["address_here", "Viewing Party", "REPLACE_ME"],
-    ["address_here", "Little Esty Shop", "REPLACE_ME"]
+    ["tldr", "TL:DR", "Python Web Application", tldr],
+    ["whether_sweater", "Whether-Sweater", "Ruby Back-End API Aggregate", whether_sweater],
+    ["tea_subs", "Tea Subs API", "Ruby Back-End API", tea_subs],
+    ["viewing_party", "Viewing Party", "Ruby Back-End DB Application", viewing_party],
+    ["little_esty_shop", "Little Esty Shop", "Ruby Back-End E-Commerce Application", little_esty_shop]
   ]
 
   return (
@@ -34,16 +37,13 @@ function Projects() {
         <div className="projects">
           <div className="projects-image-viewer">
             <div className="projects-image-viewer-content">
-              <div className="projects-image-wrapper" style={{ opacity: hoveredIndex === 0 ? 1 : 0 }}>
-                <div id="TLDR-picture" className="projects-image-size">
-                  <img className="TLDR-image" src={TLDR} alt="TLDR_image"/>
+              {projects_Array.map((slug, index) => (
+                <div key={index} className="projects-image-wrapper" style={{ opacity: hoveredIndex === index ? 1 : 0 }}>
+                  <div className="projects-image-size">
+                    <img className="project-image" src={slug[3]} alt= {slug[1] + "_image"}/>
+                  </div>
                 </div>
-              </div>
-              <div id="OTHER-wrapper" className="projects-image-wrapper" style={{ opacity: hoveredIndex === 1 ? 1 : 0 }}>
-                <div id="OTHER-picture" className="projects-image-size">
-                  <img className="OTHR-image" src={OTHER} alt="TLDR_image"/>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
           <div className="projects-list-viewer">
