2e8480cd56c53c4c7d75084ed828835f97853f65
Isaac3924 <isaacalter3924@gmail.com>
2024-09-18 17:52:04 -0600

    Refactor: Added in css stylings to help rectify and style individual files. Still being worked on.

--- src/projects/TLDR.js
+++ src/projects/TLDR.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useMenu } from "../MenuContext";
+import mainpic from '../pictures/tldr_home.png';
 
 function TLDR() {
   const { menuVisible } = useMenu();
+  /*
+  useEffect(() => {
+    //Find the app element to modify its styles
+    const appElement = document.querySelector('.app');
+    if(appElement) {
+      appElement.style.height = 'auto';
+      appElement.style.overflowY = 'auto';
+    }
+
+  }, []);
+  */
   return (
     <div className={`page ${menuVisible ? 'menu-visible' : ''}`}>
       <div className="project">
+        <div className="project-wrapper">
+          
+        </div>
+
+        <div className="title-pic">
+          <div className="title-pic-wrapper">
+            <img className="tldr-main-pic" src={mainpic} alt="TL:DR Home Page" />
+          </div>
+        </div>
         
         <div className="project-title">
           <div className="page-head">
@@ -27,8 +48,9 @@ function TLDR() {
                   <td>
                     <ul>
                       <li>Python</li>
-
-
+                      <li>TypeScript</li>
+                      <li>CSS</li>
+                      <li>HTML</li>
                     </ul>
                   </td>
 
